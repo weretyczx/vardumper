069da8a3cee2ad36819634440d461522b3db2db8
Huge <huge.f@topskyweb.com>
2018-10-19 19:12:21 +0800

    update dd conflict

--- tests/index.php
+++ tests/index.php
@@ -1,4 +1,26 @@
 <?php
 
 require 'vendor/autoload.php';
+
+// $d = function d(...$args)
+// {
+//     http_response_code(500);
+
+//     foreach ($args as $x) {
+//         (new Weretyczx\Vardumper\Dumper)->dump($x);
+//     }
+
+//     die();
+// };
+
+// runkit_method_redefine(
+//     '',
+//     'dd',
+//     '',
+//     'return "111111";',
+//     RUNKIT_ACC_PUBLIC
+// );
+
+
+// $dd
 dd('d');
